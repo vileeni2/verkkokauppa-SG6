0e3a86c21fd03c2bbde4e905e941cbd52d318313
RoosaPP <127877638+RoosaPP@users.noreply.github.com>
2023-11-20 19:08:36 +0200

    starting the shopping cart

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,18 @@ import './header.css';
 import Login from '../pages/login'; 
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Bootstrap JavaScript
+import './shopping_cart';
 
 const Header = () => {
   return (
     <header className="header-container">
       <h1>AMAZING SHOE STORE</h1>
+
+      <div className="shopping-cart">
+       <button type='button'>ostoskori</button>
+       < link to ="./shopping_cart"></link>
+      </div>
+      
       <div className="dropdown">
         <button className="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style={{ backgroundColor: '#ff3232'}}>
           Login / Register here!
---
+++ src/components/shopping_cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const shopping_cart = () => {
+  return (
+    <div>
+      <h1>Ostoskori</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default shopping_cart;
