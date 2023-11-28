fee5e3d98e00b44aba86858806d1574206d0a3a2
vileeni2 <villesinkkonen12@gmail.com>
2023-11-28 11:57:02 +0200

    Register function

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,32 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../components/Register.css';
 
 function Register() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Tässä vaiheessa voi lisätä lomakkeen tietojen käsittelyn, esim. lähettää ne palvelimelle.
+    console.log(formData);
+    // Tähän voi lisätä lisälogiikkaa tai siirtyä seuraavaan näkymään.
+  };
+
   return (
     <div className="register-container">
-    <h2>Register</h2>
-    <form>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" name="username" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-      </div>
-      <button type="submit">Register</button>
-    </form>
-  </div>
-  )
+      <h2>Register</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          />
+        </div>
+        <button type="submit" onClick={handleSubmit}>Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
